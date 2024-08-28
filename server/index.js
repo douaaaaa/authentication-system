bd7240d20176e175f90f57b72e206471e89a6c9e
Douâa Bouabdallah <bouabdallehdouaa@gmail.com>
2024-08-28 15:22:48 +0100

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 // Import the Express library
-
 import express from "express";
 // Import and configure dotenv to load environment variables from a .env file
 import "dotenv/config";
+// Import the Mongoose library to interact with MongoDB
+import mongoose from "mongoose";
 
 // Set the port from the environment variable or default to 8000 if not provided
 const PORT = process.env.PORT || 8000;
+const MONGO = process.env.MONGO;
 
 // Create an instance of an Express application
 const app = express();
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-  console.log(`the port is running on the port ${PORT}`);
-});
+// Connect to the MongoDB database using Mongoose
+mongoose
+  .connect(MONGO)
+  .then(() =>
+    // Start the server and listen on the specified port
+    app.listen(PORT, () => {
+      console.log(`the port is running on the port ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
